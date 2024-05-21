7ec266d2cd540c9a85d3032ed790b16f5ae45a99
Mustafa AlAmin Jaber <mjaber@propke.com>
2024-05-21 23:31:02 +0300

    create the database tables with corresponding foreign keys

---
+++ database/migrations/2024_05_21_202312_create_items_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image_path');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
+            $table->unsignedBigInteger('content_format_id');
+            $table->foreign('content_format_id')->references('id')->on('content_formats')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('external_url');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
